db89a90c3b935f3f353198456164f0615aa053c8
danielftg <130905505+danielftg@users.noreply.github.com>
2025-07-10 16:37:12 +0200

    Add platt scaling and isotonic regression

--- components/architectures.py
+++ components/architectures.py
@@ -1,12 +1,13 @@
 import components.config as cf
 import numpy as np 
-import sklearn 
+from sklearn.linear_model import LogisticRegression
+from sklearn.isotonic import IsotonicRegression
 import xgboost
 import catboost
 import lightgbm
 import keras
 import tabpfn
-import betacal
+from betacal import BetaCalibration
 import venn_abers
 import pearsonify
 
@@ -21,15 +22,24 @@ class PreProcessing:
     def apply(self, x):
         raise NotImplementedError
 
+
 class PostProcessing:
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, learner_class,instatiator_fn, fit_fn, predict_prob_fn):
+        self.learner_class = learner_class
+        self.instatiator_fn = instatiator_fn
+        self.learner = None
+        self._fit_fn = fit_fn
+        self._predict_prob_fn = predict_prob_fn
     
-    def fit(self, y_in, y_out):
-        raise NotImplementedError
+    def instantiate(self, meta_data:dict):
+        cf.logger.info(f"Post-processing instantiated with:{self.instatiator_fn(meta_data)}")
+        self.learner = self.learner_class(**self.instatiator_fn(meta_data))
+
+    def fit(self, y_instance, y_target):
+        self._fit_fn(self.learner, y_instance, y_target)
     
     def predict_prob(self, y):
-        raise NotImplementedError
+        return self._predict_prob_fn(self.learner, y)
 
 
 class Model:
@@ -45,7 +55,7 @@ class Model:
         self.learner = self.learner_class(**self.instatiator_fn(meta_data))
 
     def fit(self, x, y):
-        return self._fit_fn(self.learner, x, y)
+        self._fit_fn(self.learner, x, y)
     
     def predict_prob(self, x):
         return self._predict_prob_fn(self.learner, x)
@@ -70,16 +80,21 @@ class Architecture:
     def fit(self, meta_data, x_train, y_train, x_calibration=None, y_calibration=None):
         
         self.model.instantiate(meta_data)
-        
         self.model.fit(x_train, y_train)
 
-        if self.post_processing:
-            if self.calibration_set:
-                y_cal_prob = self.model.predict_prob(x_calibration)
-                self.post_processing.train(y_cal_prob, y_calibration)
-            else:
-                y_cal_prob = self.model.predict_prob(x_train)
-                self.post_processing.train(y_cal_prob, y_train)
+        if not (self.post_processing is None):
+            y_cal_prob = self.model.predict_prob(x_calibration if self.calibration_set else x_train)
+            
+            y_cal_prob = np.asarray(y_cal_prob)
+            
+            if y_cal_prob.ndim == 2 and y_cal_prob.shape[1] == 2:
+                y_cal_prob =  y_cal_prob[:, 1]
+            
+            if y_cal_prob.ndim == 1:
+                y_cal_prob = y_cal_prob.reshape(-1,1)
+
+            self.post_processing.instantiate(meta_data)
+            self.post_processing.fit(y_cal_prob, y_calibration if self.calibration_set else y_train)
 
 
     def predict_prob(self, x):
@@ -90,7 +105,13 @@ class Architecture:
             y_prob =  y_prob[:, 1]
     
         if self.post_processing:
+            if y_prob.ndim == 1:
+                y_prob = y_prob.reshape(-1,1)
+
             y_prob = self.post_processing.predict_prob(y_prob)
+            
+            if y_prob.ndim == 2 and y_prob.shape[1] == 2:
+                y_prob =  y_prob[:, 1]
 
         return y_prob
     
@@ -103,11 +124,6 @@ class Architecture:
             return np.where(y_prob >= 0.5, 1, 0)
         else:
             return np.argmax(y_prob, axis=1) + 1
-
-        
-
-        
-     
 
 
 class ArchitectureSuite:
@@ -126,16 +142,52 @@ class ArchitectureSuite:
 
     def init_v1(self):
         archs = []
-        std_fit = lambda learner, x, y: learner.fit(x, y)
-        std_predict_prob = lambda learner, x: learner.predict_proba(x)
+
+        md_std_fit = lambda learner, x, y: learner.fit(x, y)
+        md_std_predict_prob = lambda learner, x: learner.predict_proba(x)
         
         cb_instantiator = lambda meta_data: {"random_seed":SEED, "verbose":False, "cat_features":meta_data["cat_features"]}
         cb = Model(learner_class=catboost.CatBoostClassifier
                    ,instatiator_fn=cb_instantiator
-                   ,fit_fn=std_fit
-                   ,predict_prob_fn=std_predict_prob
+                   ,fit_fn=md_std_fit
+                   ,predict_prob_fn=md_std_predict_prob
         ) 
+
+        pp_std_fit = lambda learner, y_in, y_ta: learner.fit(y_in, y_ta)
+        pp_std_predict_prob = lambda learner, y: learner.predict_proba(y)
+        pp_std_predict = lambda learner, y: learner.predict(y)
+        
+        lr_instantiator = lambda meta_data: {"random_state":SEED}
+        platt_scaling = PostProcessing(
+            learner_class = LogisticRegression
+            ,instatiator_fn = lr_instantiator
+            ,fit_fn = pp_std_fit
+            ,predict_prob_fn = pp_std_predict_prob
+            )
+        
+        ir_instantiator = lambda meta_data: {"out_of_bounds":"clip"}
+        isotonic_regression = PostProcessing(
+            learner_class = IsotonicRegression
+            ,instatiator_fn = lr_instantiator
+            ,fit_fn = pp_std_fit
+            ,predict_prob_fn = pp_std_predict
+            )
+        
+
+
         archs.append(Architecture(name="catboost" ,model=cb))
+        archs.append(Architecture(
+                 name="catboost.platt_scaling"
+                 ,model=cb
+                 ,calibration_set=True
+                 ,post_processing=platt_scaling
+        ))
+        archs.append(Architecture(
+                 name="catboost.isotonic_regression"
+                 ,model=cb
+                 ,calibration_set=True
+                 ,post_processing=isotonic_regression
+        ))
 
-
+        
         self.architectures = archs
